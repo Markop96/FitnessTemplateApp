a80a6bee10dd43734c1a0f2a8452b791d3d1a0dd
vaš_username_saa <markop1609@gmail.com>
2025-03-14 00:12:02 +0100

    add

---
+++ wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+        direction: 'top'
+    });
+});
+
+window.onscroll = function () {
+    var scrollButton = document.querySelector('.button12');
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollButton.style.display = "flex";
+    } else {
+        scrollButton.style.display = "none";
+    }
+};
+
+document.getElementById("scrollToTop").addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
